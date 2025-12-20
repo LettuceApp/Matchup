2b5c19462b2c0ba6b798f445f6a0712ef8de3bbf
Cordell Jenkins <cordelljenkins1914@gmail.com>
2025-12-20 13:07:57 -0600

    dockerfile and definitions fixes

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,7 +1,25 @@
-from dagster import Definitions, load_assets_from_modules
+# definitions.py
+from dagster import (
+    Definitions,
+    load_assets_from_modules,
+    define_asset_job,
+    in_process_executor,
+)
 import assets as matchup_assets
 
-# Load all assets from assets.py automatically.
+# Load all assets from assets.py
 all_assets = load_assets_from_modules([matchup_assets])
 
-defs = Definitions(assets=all_assets)
+# Define a job that materializes all assets in-process
+all_assets_job = (
+    define_asset_job(
+        name="all_assets_job",
+        selection="*",
+    )
+    .configure_executor_def(in_process_executor)
+)
+
+defs = Definitions(
+    assets=all_assets,
+    jobs=[all_assets_job],
+)
