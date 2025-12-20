6bfa7e94ac3022617f09f64597e3f4f3aeeee17c
Cordell Jenkins <cordelljenkins1914@gmail.com>
2025-12-20 13:17:03 -0600

    fix definitions

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,25 +1,7 @@
-# definitions.py
-from dagster import (
-    Definitions,
-    load_assets_from_modules,
-    define_asset_job,
-    in_process_executor,
-)
+from dagster import Definitions, load_assets_from_modules
 import assets as matchup_assets
 
-# Load all assets from assets.py
+# Load all assets from assets.py automatically.
 all_assets = load_assets_from_modules([matchup_assets])
 
-# Define a job that materializes all assets in-process
-all_assets_job = (
-    define_asset_job(
-        name="all_assets_job",
-        selection="*",
-    )
-    .configure_executor_def(in_process_executor)
-)
-
-defs = Definitions(
-    assets=all_assets,
-    jobs=[all_assets_job],
-)
+defs = Definitions(assets=all_assets)
