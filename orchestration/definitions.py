e62e935e6875a5fa46e3fd7e2f2998d1088d2799
Cordell Jenkins <cordelljenkins1914@gmail.com>
2025-12-20 13:46:57 -0600

    bug fix

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -1,7 +1,11 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, in_process_executor
 import assets as matchup_assets
 
 # Load all assets from assets.py automatically.
 all_assets = load_assets_from_modules([matchup_assets])
 
-defs = Definitions(assets=all_assets)
+# Use in-process executor as the default for all jobs / asset materializations
+defs = Definitions(
+    assets=all_assets,
+    executor=in_process_executor,
+)
