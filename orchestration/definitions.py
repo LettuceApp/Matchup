62142e2595446c90b67b098b1a4563c01aba6ae2
Cordell Jenkins <cordelljenkins1914@gmail.com>
2025-12-21 16:29:43 -0600

    created popular matchup job

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -7,24 +7,24 @@ from dagster import (
 )
 import assets as matchup_assets
 
-# Load all assets
+# Load all the assets
 all_assets = load_assets_from_modules([matchup_assets])
 
-# Create a job that materializes ONLY the popular_matchup asset
-popular_matchup_job = define_asset_job(
-    name="popular_matchup_job",
-    selection=["popular_matchup"]  # <-- must match your @asset name
+# Job must reference the ACTUAL asset name
+popular_matchups_job = define_asset_job(
+    name="popular_matchups_job",
+    selection=["popular_matchups"],
 )
 
-# Run the job every hour
-popular_matchup_hourly_schedule = ScheduleDefinition(
-    job=popular_matchup_job,
-    cron_schedule="@hourly"
+# Run this job hourly
+popular_matchups_hourly_schedule = ScheduleDefinition(
+    job=popular_matchups_job,
+    cron_schedule="@hourly",
 )
 
 defs = Definitions(
     assets=all_assets,
-    schedules=[popular_matchup_hourly_schedule],
-    jobs=[popular_matchup_job],
+    jobs=[popular_matchups_job],
+    schedules=[popular_matchups_hourly_schedule],
     executor=in_process_executor,
 )
